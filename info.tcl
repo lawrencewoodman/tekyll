2a3851794878b435a474d95dee381443a640642f
Lawrence Woodman <lwoodman@vlifesystems.com>
2018-06-20 13:25:30 +0100

    Add search for tcllib versions using apt-cache

--- info.tcl
+++ info.tcl
@@ -2,10 +2,10 @@ puts "auto_path: $auto_path"
 
 foreach dir $auto_path {
   puts "$dir\n======================\n"
-  foreach subDir [glob -directory $dir *] {
+  foreach subDir [lsort [glob -directory $dir *]] {
     if {[string first tcllib $subDir] >= 0} {
       puts "****$subDir"
-      foreach f [glob -directory [file join $dir $subDir] *] {
+      foreach f [lsort [[glob -directory [file join $dir $subDir] *]] {
         if {[string first markdown $f] >= 0} {
           puts "********$f"
         } else {
