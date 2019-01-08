08685d724eefa80a359a441469446b9f88e72dbf
Lawrence Woodman <lwoodman@vlifesystems.com>
2019-01-08 12:51:35 +0000

    Make init/main.tcl easier to follow

--- init/main.tcl
+++ init/main.tcl
@@ -1,7 +1,5 @@
-set tekyllTcl "# File created using tekyll: https://github.com/lawrencewoodman/tekyll\n\n"
-append tekyllTcl [
-  ornament [regsub -line -all {^#>(.*)$} \
-                   [read -directory [dir root] main.tcl] {\1}]
-]
-
+set header "# File created using tekyll: https://github.com/lawrencewoodman/tekyll\n\n"
+set mainTcl [read -directory [dir root] main.tcl]
+set uncommentedOrnamentText [regsub -line -all {^#>(.*)$} $mainTcl {\1}]
+set tekyllTcl [append $header [ornament $uncommentedOrnamentText]]
 write [file join [dir build] tekyll.tcl] $tekyllTcl
