15ec3264ac72b30b7f3ea3ba9cf3aa96798f1c88
Lawrence Woodman <lwoodman@vlifesystems.com>
2019-01-08 11:59:49 +0000

    Create tests/all.tcl to run entire testsuite

--- init/main.tcl
+++ init/main.tcl
@@ -1,4 +1,7 @@
-write [file join [dir build] tekyll.tcl] [
+set tekyllTcl "# File created using tekyll: https://github.com/lawrencewoodman/tekyll\n\n"
+append tekyllTcl [
   ornament [regsub -line -all {^#>(.*)$} \
                    [read -directory [dir root] main.tcl] {\1}]
 ]
+
+write [file join [dir build] tekyll.tcl] $tekyllTcl
---
+++ tests/all.tcl
@@ -0,0 +1,5 @@
+package require Tcl 8.6
+package require tcltest
+::tcltest::configure -testdir [file dirname [file normalize [info script]]]
+::tcltest::configure {*}$argv
+::tcltest::runAllTests
