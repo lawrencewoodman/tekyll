d2023294e46c2d7326d2da082f050164b7c5ddbf
Lawrence Woodman <lwoodman@vlifesystems.com>
2020-04-17 09:52:11 +0100

    Ensure that all.tcl test exits with correct value

--- tests/all.tcl
+++ tests/all.tcl
@@ -2,4 +2,4 @@ package require Tcl 8.6
 package require tcltest
 ::tcltest::configure -testdir [file dirname [file normalize [info script]]]
 ::tcltest::configure {*}$argv
-::tcltest::runAllTests
+exit [tcltest::runAllTests]
